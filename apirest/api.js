cf554a3277015e60db9547887887a1d166d71c68
Víctor Martínez <ahal_tocob@hotmail.com>
2022-05-23 14:59:24 -0500

    Se agrega el swagger y la colección de postman

--- apirest/api.js
+++ apirest/api.js
@@ -2,11 +2,13 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const cors = require('cors');
-
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 const app = express();
 
 const port = process.env.PORT || 4000;
 
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(cors())
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -16,3 +18,10 @@ app.use(require("./routes/rest"));
 app.listen(port, () => {
     console.log("Servidor iniciando puerto en " + port);
 });
+
+/*
+eric.sanchez@udlondres.com
+Enviar reporte de la aplicación
+
+https://www.heroku.com/
+*/
